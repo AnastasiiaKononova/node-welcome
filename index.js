a00a5bc1a65807043be1f4f317c0d7a0847bc12f
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-01-09 15:49:17 +0200

    user signIn

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
-const http = require('http')
+const http = require("http");
 
-const server = http.createServer(requestListener) //створюємо сервер, який реагує на запити та передаємо функцію-обробник запитів
+const users = new Map();
 
-function requestListener (request, response) {
-  console.log(request.url, request.method)
-  if (request.method === 'GET') {
-    if (request.url === '/') {
+function signIn(userObj) {
+  users.set(userObj.email, userObj);
+}
+
+function getListUsers() {
+  return [...users.values()];
+}
+
+const server = http.createServer(requestListener); // створюємо сервер, який реагує на запити та передаємо функцію-обробник запитів
+
+function requestListener(request, response) {
+  console.log(request.url, request.method);
+  if (request.method === "GET") {
+    if (request.url === "/") {
       // відповісти "хеллоу ворлд"
-      response.statusCode = 200
-      response.statusMessage = 'OK'
-      response.end('Hello, world')
-    } else if (request.url === '/about') {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.end("Hello, world");
+    } else if (request.url === "/about") {
       // відповісти "сторінка про наш чудовий сайт"
-      response.statusCode = 200
-      response.statusMessage = 'OK'
+      response.statusCode = 200;
+      response.statusMessage = "OK";
       response.end(
-        '<html><body>This is a page about our super site</body></html>'
-      )
+        "<html><body>This is a page about our super site</body></html>"
+      );
+    } else if (request.url === "/users") {
+      // відповісти на запит списком вже зареєстрованих юзерів
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      const usersList = getListUsers();
+      response.end(JSON.stringify(usersList));
     } else {
-      response.statusCode = 404
-      response.statusMessage = 'Not Found'
-      response.end('this page not found')
+      response.statusCode = 404;
+      response.statusMessage = "Not Found";
+      response.end("this page not found");
     }
   }
-  if (request.method === 'POST') {
-    if ((request.url = '/')) {
-      response.statusCode = 400
-      response.statusMessage = 'Forbidden'
-      response.end('This page has not support this operation')
+
+  if (request.method === "POST") {
+    if (request.url === "/") {
+      response.statusCode = 400;
+      response.statusMessage = "Forbidden";
+      response.end("This page has not support this operation");
+    } else if (request.url === "/users") {
+      // Задача - прийняти з запиту дані юзера і відповісти успіхом
+      // Зчитуємо дані і зклеюємо їх докупи
+      let string = "";
+      request.on("data", (chunk) => {
+        string += chunk;
+      });
+      request.on("end", () => {
+        // отут всі дані вже завантажено і сталася фінальна подія, яка сигналізує про завершення завантаження інфи
+
+        // можемо щось робити з тим, що вже лежить в глобальній змінній string
+        const userObj = JSON.parse(string);
+        console.log(userObj);
+        signIn(userObj);
+        // таска: відповісти: John Doe, ви зареєстровані в системі
+        const resString = `${userObj.username} ${userObj.lastName} - you have successfully sign in`;
+        response.statusCode = 201;
+        response.statusMessage = "Created";
+        response.end(resString);
+      });
+    } else {
+      response.statusCode = 404;
+      response.statusMessage = "Not Found";
+      response.end("this page not found");
     }
   }
 }
-server.listen(5000)
+
+server.listen(5000);
